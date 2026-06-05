8d6235aa5083607d3fe27bf7ec3b488e9f96a5e9
Palakaggarwal GLB_25112CN377 <palakagg040407@gmail.com>
2026-06-05 16:30:05 +0530

    Create Question17.java

---
+++ Day 5/Question17.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Question17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        if(num<=0){
+            System.out.println(num + " is not a perfect number.");
+        }
+        else{
+            for(int i = 1 ; i< num ;i++){
+                if(num%i==0){
+                    sum += i;
+                }
+            }
+        
+        if(num == sum){
+            System.out.println(num + " is a perfect number.");
+        }else{
+            System.out.println(num + " is not a perfect number.");
+        }
+        }
+    
+    sc.close();
+    }
+    
+}
