3c4ede391150dc841a6ad8d6f014054fae62eb14
Palakaggarwal GLB_25112CN377 <palakagg040407@gmail.com>
2026-06-11 13:46:13 +0530

    Create Function_of_maximum.java

---
+++ Day 11/Function_of_maximum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Function_of_maximum{
+
+    public static int maximum(int num1 , int num2){
+        if(num1 > num2)
+            return num1;
+        else
+            return num2;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number : ");
+        int num2 = sc.nextInt();
+
+        int result = maximum(num1 , num2);
+        System.out.println("The maximum number is " +result);
+
+        sc.close();
+
+    }
+}
